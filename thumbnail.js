f8a797412defd659cfd2c09451861cdd595da349
cikeXT <100738179+cikeXT@users.noreply.github.com>
2022-03-07 14:48:47 +0800

    Add files via upload

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // 你的所有代码都应该写在这里
-	let targetImage = document.querySelector("#smart-image");
+document.addEventListener("DOMContentLoaded", function(event)
+ {let targetImage = document.querySelector("#smart-image");
 	targetImage.addEventListener("click", function() {
     targetImage.classList.remove("small");
 });
